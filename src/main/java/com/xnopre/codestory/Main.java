c459a9968ff0ce79e26310e8af04904d9fe2b93b
Xavier NOPRE <xnopre@gmail.com>
2013-01-16 19:38:19 +0100

    Pour lance le programme : récupération du port dans la variable d'environnement PORT via "getenv"

--- src/main/java/com/xnopre/codestory/Main.java
+++ src/main/java/com/xnopre/codestory/Main.java
@@ -3,7 +3,25 @@ package com.xnopre.codestory;
 public class Main {
 
 	public static void main(String[] args) {
-		new XnopreCodestoryWebService(8081, new MainController()).startAndWait();
+		new XnopreCodestoryWebService(getEnvPort(), new MainController())
+				.startAndWait();
+	}
+
+	private static int getEnvPort() {
+		String envPort = System.getenv("PORT");
+		if (envPort == null) {
+			throw new IllegalStateException("Environment variable PORT is null");
+		}
+		if (envPort.length() == 0) {
+			throw new IllegalStateException(
+					"Environment variable PORT is empty");
+		}
+		try {
+			return Integer.parseInt(envPort);
+		} catch (NumberFormatException e) {
+			throw new IllegalStateException(
+					"Environment variable PORT is not an integer", e);
+		}
 	}
 
 }
