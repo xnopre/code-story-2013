c2f73eb6a7113726425ee2ea631fa83d585884c9
Xavier NOPRE <xnopre@gmail.com>
2013-01-17 07:37:49 +0100

    Ajout du script pour déployer

---
+++ deploy.bat
@@ -0,0 +1,1 @@
+bees app:deploy -a codestory/xnopre -t java  -R class=com.xnopre.codestory.Main -R classpath=codestory2013-0.0.1-SNAPSHOT-jar-with-dependencies.jar target\codestory2013-0.0.1-SNAPSHOT-jar-with-dependencies.jar 
